87d6236397a3f4bead9b3a80ba2838ad33b05c8f
ilan-lang <ilan@syv.co.il>
2026-05-29 23:13:13 +0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,24 +1,30 @@
-const CACHE = 'sieners-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'sieners-v3';
+const ASSETS = [
+    '/sieners-pricing/',
+    '/sieners-pricing/index.html',
+    '/sieners-pricing/manifest.json',
+    '/sieners-pricing/icons/icon-192.png',
+    '/sieners-pricing/icons/icon-512.png'
+  ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
+    e.waitUntil(
+          caches.open(CACHE).then(c => c.addAll(ASSETS))
+        );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+    e.waitUntil(
+          caches.keys().then(keys =>
+                  Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+                                 )
+        );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+    e.respondWith(
+          caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/sieners-pricing/')))
+        );
 });
